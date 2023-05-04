5c04eeb4971a4e427c061441cfb351d35b5484c2
HarshinirRaj <89665770+HarshinirRaj@users.noreply.github.com>
2023-05-04 16:09:27 -0500

    Create ExpenseData.js

---
+++ budget/src/data/ExpenseData.js
@@ -0,0 +1,38 @@
+export let expenses = [
+    {
+      "id": "1",
+      "text": "Doctors Appointment",
+      "day": "2023-03-05",
+      "amount": 50
+    },
+    {
+      "id": "2",
+      "text": "Groceries",
+      "day" : "2023-03-06",
+      "amount": 36
+    },
+    {
+      "id": "3",
+      "text": "Furniture",
+      "day": "2023-04-07",
+      "amount": 85
+    },
+    {
+      "id": "4",
+      "text": "House Rent",
+      "day": "2023-03-05",
+      "amount": 1500
+    },
+    {
+      "id": "5",
+      "text": "Gas & Electricity",
+      "day" : "2023-04-06",
+      "amount": 55
+    },
+    {
+      "id": "6",
+      "text": "Travel",
+      "day": "2023-03-07",
+      "amount": 2000
+    },
+  ]
