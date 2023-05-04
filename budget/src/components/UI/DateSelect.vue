da8764a4a3a8d6062369db828bb6920292a0fdb6
HarshinirRaj <89665770+HarshinirRaj@users.noreply.github.com>
2023-05-04 16:12:58 -0500

    Add files via upload

---
+++ budget/src/components/UI/DateSelect.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="month-selector">
+      <label for="month-dropdown">Month : </label>
+      <select id="month-dropdown" v-model="selectedMonth" @change="handleMonthChange">
+        <option v-for="(month, index) in months" :key="index" :value="index + 1">{{ month }}</option>
+      </select>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name : 'DateSelect-Component',
+    data() {
+      return {
+        selectedMonth: 3,
+        months: [
+          'January', 'February', 'March', 'April', 'May', 'June',
+          'July', 'August', 'September', 'October', 'November', 'December',
+        ],
+      };
+    },
+    methods: {
+        handleMonthChange() {
+            this.$emit('month-selected', this.selectedMonth);
+        },
+    },
+  };
+  </script>
+
+<style scoped>
+  label{
+    font-weight: bold;
+  }
+  .month-selector{
+    margin-top: 20px;
+  }
+</style>
+  
